2e7c79dd3b508fb676e08257ad8784e95f45e3ec
Kaiyuan Yu <kaiyuany@usc.edu>
2015-11-15 02:58:08 -0800

    front end completed

---
+++ assets/js/index.js
@@ -0,0 +1,28 @@
+document.getElementById('convert_file').onclick = function(){
+  document.getElementById('status').innerHTML = "Status: Loading Success!";
+}
+
+var img;
+function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                // $('in')
+                //     .attr('src', e.target.result)
+                //     .width(350)
+                //     .height(300);
+                img = document.getElementById('in');
+                img.src = e.target.result;
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        }
+}
+
+function clickConvert(){
+    document.getElementById("convert_file").click();
+}
+function clickLoad(){
+    document.getElementById("my_file").click();
+}
